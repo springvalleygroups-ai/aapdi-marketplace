fb2a96b2d49b79081cee75e7fd37cd9b11e412a4
springvalleygroups-ai <springvalleygroups@gmail.com>
2026-06-15 23:53:25 +0530

    Update layout.js

--- app/app/layout.js
+++ app/app/layout.js
@@ -1,12 +1,1 @@
-export const metadata = {
-  title: "Aapdi Marketplace",
-  description: "Multi Vendor Marketplace",
-};
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
